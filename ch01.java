bfdeab294d57b092070de4333b3f380484b7fd39
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-16 14:03:26 +0800

    Add files via upload

---
+++ ch01.java
@@ -0,0 +1,30 @@
+package ch01;
+
+public class ch01 {
+
+	public static void main(String[] args) {
+		System.out.println("位元組整數所佔的記憶體範圍::"+Byte.SIZE/8+"BYTE.");
+		System.out.println("位元組整數的範圍:"+Byte.MIN_VALUE+"~"+Byte.MAX_VALUE);
+		
+		System.out.println("短整數所佔的記憶體範圍::"+Short.SIZE/8+"BYTE.");
+		System.out.println("短整數的範圍:"+Short.MIN_VALUE+"~"+Short.MAX_VALUE);
+		
+		System.out.println("整數所佔的記憶體範圍::"+Integer.SIZE/8+"BYTE.");
+		System.out.println("整數的範圍:"+Integer.MIN_VALUE+"~"+Integer.MAX_VALUE);
+		
+		System.out.println("長整數所佔的記憶體範圍::"+Long.SIZE/8+"BYTE.");
+		System.out.println("長整數的範圍:"+Long.MIN_VALUE+"~"+Long.MAX_VALUE);
+		
+		System.out.println(" 單經度福點數所佔的記憶體範圍::"+Float.SIZE/8+"BYTE.");
+		System.out.println("單經度福點數數的範圍:"+Float.MIN_VALUE+"~"+Float.MAX_VALUE);
+		
+		System.out.println(" 倍經度福點數所佔的記憶體範圍::"+Double.SIZE/8+"BYTE.");
+		System.out.println("倍經度福點數數的範圍:"+Double.MIN_VALUE+"~"+Double.MAX_VALUE);
+		
+		
+		
+		
+		
+	}
+
+}
