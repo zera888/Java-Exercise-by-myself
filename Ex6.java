b02ffe60a3ca86f2c1c39112e1fdddb1e4bda179
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-16 14:05:00 +0800

    Add files via upload

---
+++ Ex6.java
@@ -0,0 +1,11 @@
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		int a=1, b=2, c=3,avg;
+		avg=(int)(a*0.3+b*0.3+c*0.4)/3;
+		System.out.println("a= "+(int)a*0.3+" , b= "+(int)b*0.3+" , c= "+(int)c*0.4);
+		System.out.println("avg= "+avg);
+	}
+
+}
