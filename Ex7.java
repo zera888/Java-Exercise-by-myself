cf244e6df7e40335fb0975a4ae0af078ec526c24
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-16 14:05:17 +0800

    Add files via upload

---
+++ Ex7.java
@@ -0,0 +1,13 @@
+
+public class Ex7 {
+
+	public static void main(String[] args) {
+		int a=1, b=2, c=3;
+		float avg;
+		avg=(float)(a*0.3+b*0.3+c*0.4)/3;
+		System.out.println("a= "+a*0.3+" , b= "+b*0.3+" , c= "+c*0.4);
+		System.out.println("avg= "+avg);
+
+	}
+
+}
