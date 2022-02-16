6d1e7c098201206813262302170e36a30181b2e1
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-16 14:05:36 +0800

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,18 @@
+package ch03;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		String name="邏輯林";
+		int age=28;
+		char blood='A';
+		float height=168.5f;
+		double money=12345678.0;
+		System.out.print("12345678901234567890456789\n");
+		System.out.println("我的名字叫"+name+" \t今年"+age+"歲");
+		System.out.print("血型是"+blood+"\t\t身高"+height+"\t\t");
+		System.out.println("銀行存款"+money+"元");
+		
+	}
+		
+}
