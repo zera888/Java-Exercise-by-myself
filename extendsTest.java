fefb894260f57d5fe9bcf2d0080d019075432622
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-18 16:45:57 +0800

    Add files via upload

---
+++ extendsTest.java
@@ -0,0 +1,16 @@
+class Test {
+	private int a;
+	
+	public int getInt() {
+		return a;
+	}
+}
+public class son extendsTest {}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
