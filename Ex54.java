ba17b62af8ba2099ce6e8d5488a15900a9329c0d
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-18 16:25:58 +0800

    Add files via upload

---
+++ Ex54.java
@@ -0,0 +1,13 @@
+
+class Ex54 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+		
+		x=10/0;
+		//分母是0是數學中的錯誤.此處未放在try區段.
+		System.out.println("x= "+x);
+	}
+
+}
