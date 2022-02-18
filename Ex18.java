3f0c8b341862e65d42306ed8516040595988bfe2
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-18 16:12:31 +0800

    Add files via upload

---
+++ Ex18.java
@@ -0,0 +1,15 @@
+class Mynumber {   // 類別程序
+	public int i;  //限制修飾子public的應用
+}
+
+public class Ex18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Mynumber M = new Mynumber();
+			
+			M.i = 10;
+			System.out.println("Public Variable M.i="+ M.i);
+	}
+
+}
