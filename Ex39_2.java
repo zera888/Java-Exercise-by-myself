364e8b781865d0a671258d4e0aef1177f57934e3
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-18 16:20:36 +0800

    Add files via upload

---
+++ Ex39_2.java
@@ -0,0 +1,16 @@
+
+ class Ex39_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int i=5;
+		final int j;
+		
+		j=20;
+		
+				
+		System.out.println("i= "+i);
+		System.out.println("j= "+j);
+	}
+
+}
