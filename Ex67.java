fc5953a98d5400ffa8f5b9b287e6c3ae19e06351
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-18 16:31:08 +0800

    Add files via upload

---
+++ Ex67.java
@@ -0,0 +1,28 @@
+class myException extends Exception{
+	myException(String msg){
+		super(msg);
+	}
+}
+
+class Ex67 {
+
+	public static void main(String[] args) {
+		myException f= new myException("DIY Message");
+		
+		int x,y;
+		
+		try {
+			x=10;
+			y=0;
+			x=10/0;
+			System.out.println("x= "+x);
+		}
+		
+		catch (ArithmeticException e) {
+			System.out.println("In Built Message :"+e.getMessage());
+			System.out.println("In myException Message :"+f.getMessage());
+			
+		}
+	}
+
+}
