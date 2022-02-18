a4b51a007841d9855d606029f452f020bfbc3fad
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-18 16:47:31 +0800

    Add files via upload

---
+++ Mynumber.java
@@ -0,0 +1,12 @@
+class Mynumber{
+	 int number;
+	public int i;
+
+
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mynumber a = new Mynumber();
+		a.number = 10;
+		System.out.println("a.number = "+a.number);
+}
+}
