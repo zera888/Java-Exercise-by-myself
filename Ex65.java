ae7e924d24cb8d329eceee9cf1f736d2599acf5a
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-18 16:29:55 +0800

    Add files via upload

---
+++ Ex65.java
@@ -0,0 +1,29 @@
+class myException extends Exception{
+	myException(){
+		super("myException Message");
+	}
+}
+
+
+class Ex65 {
+
+	public static void main(String[] args) {
+		myException f= new myException();
+		
+		int x,y;
+		
+		try {
+			x=10;
+			y=0;
+			x=10/0;
+			System.out.println("x ="+x);
+		}
+		
+		catch (ArithmeticException e) {
+			System.out.println("In Built Message :" +e.getMessage());
+			System.out.println("In myException Message :"+f.getMessage());
+			
+		}
+	}
+
+}
