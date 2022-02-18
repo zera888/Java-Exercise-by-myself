b5eb451d2b01ee25e45a16fef28ed9762719a951
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-18 16:47:10 +0800

    Add files via upload

---
+++ MyEnc.java
@@ -0,0 +1,20 @@
+public class DemoEnc{
+	public static void main (String[] args) {
+		MyEnc acc=new MyEnc();
+		acc.setMoney(10000);
+		System.out.println("$"+acc.setMoney());
+		
+	}
+}
+class MyEnc {
+	private int money;
+	
+}
+public class MyEnc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
