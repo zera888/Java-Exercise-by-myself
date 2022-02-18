957b93a7e1c73eedd923032aac1659984d1e70a9
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-18 16:48:00 +0800

    Add files via upload

--- Mynumber_08.java
+++ Mynumber_08.java
@@ -1,17 +1,18 @@
+ class Mynumber {
+	 int i;
+  
 
-public class Mynumber_08 {
-	public int i;
-	
 	public static void main(String[] args) {
 		int j;
-		Mynumber a;
+		Mynumber a;  //宣告類別
 		
+			
+		a = new Mynumber();   //建立類別
+		a.i=100;
 		j=5;
-		a = new Mynumber();
-		a.i = 10;
 		System.out.println("j="+j);
 		System.out.println("a.i="+a.i);
+		
 	}
-
 
 }
