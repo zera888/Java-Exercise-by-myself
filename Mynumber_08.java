0d1f6a1cd9e80e11559688ca9dff78d499554a37
zera888 <77621980+zera888@users.noreply.github.com>
2022-02-18 16:47:42 +0800

    Add files via upload

---
+++ Mynumber_08.java
@@ -0,0 +1,17 @@
+
+public class Mynumber_08 {
+	public int i;
+	
+	public static void main(String[] args) {
+		int j;
+		Mynumber a;
+		
+		j=5;
+		a = new Mynumber();
+		a.i = 10;
+		System.out.println("j="+j);
+		System.out.println("a.i="+a.i);
+	}
+
+
+}
